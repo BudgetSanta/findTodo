169856d0642f2ee74a4d639be735afebc96f89bc
Jared Steiner <steiner.jared1996@gmail.com>
2017-09-17 00:48:24 +1000

    Setting up basic file
    Ideas and sort of structure i guess

---
+++ fileTodos.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(int argc, char *argc[]) {
+
+    /*
+        Revision 1
+            - Search through file give as command line arg
+            - Find single lines with TODOs in them
+            - print out on single lines, info
+                line No. > TODO the rest of the line comment
+
+        Ideas for future
+            - Multi line TODOs
+            - this file can be used to search through folders or projects
+                    FILE.c
+                1 > TODO fix this
+                8 > TODO fix that
+                9 > TODO etc
+            - Possibly also get branch name?
+    */
+
+    return 0;
+}
