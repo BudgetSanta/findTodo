bddff00cc3c352cee40fb435cb572d8a114d02fb
Jared Steiner <steiner.jared1996@gmail.com>
2017-09-18 11:58:45 +1000

    Basic working. Function takes filename outputs oneline todos with no.

--- fileTodos.c
+++ fileTodos.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char *argc[]) {
+#define MAX_BUFF 1024
+#define MAX_FNAME 100
+
+// PROTOTYPES
+void printTodos(FILE *fp);
+
+int main(int argc, char *argv[]) {
 
     /*
         Revision 1
@@ -19,5 +27,32 @@ int main(int argc, char *argc[]) {
             - Possibly also get branch name?
     */
 
+    char filename[MAX_FNAME];                               // FILENAME
+
+    if (argc == 2) {
+                strcpy(filename, argv[1]);
+    } else {
+        printf("ERROR: No file supplied\nUsage: %s <file>\n", argv[0]);
+        return 1;                                           // EXIT FAIL
+    }
+
+    char const* const fileName = argv[1]; /* should check that argc > 1 */
+    FILE* file = fopen(fileName, "r"); /* should check the result */
+    printTodos(file);                                         // PRINT TODOs
+    fclose(file);                                             // CLOSE FILE
     return 0;
+}
+
+void printTodos(FILE *fp) {
+
+    char iter_buff[MAX_BUFF];                                   // Line buffer
+    int  lineCount = 1;                                         // Line count
+    char *ret;                                                  // Pointer to TODO in str
+
+    while (fgets(iter_buff, sizeof(iter_buff), fp)) {
+        if ( (ret = strstr(iter_buff, "// TODO")) ) {
+            printf("[%d] %s", lineCount, ret);
+        }
+        lineCount++;
+    }
 }
